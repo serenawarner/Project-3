c6681d628d824ad183f36bc64870bad2dbe658fc
critterday <tjons26@wooster.edu>
2025-04-18 13:34:43 -0400

    this code doesnt work and idk why

---
+++ frontendcodeuhhidk.py
@@ -0,0 +1,24 @@
+i = "0"
+while i != "00":
+    
+    # generates array of command and syntaxes
+
+    cmd = input()
+    cmdarr = str(input.split())
+    opener = cmdarr[0]
+    syntax1 = cmdarr[1]
+    syntax2 = cmdarr[2]
+
+    #test cases
+    print(opener)
+    print(syntax1)
+    print(syntax2)
+
+    
+
+
+    # checks for recognized command
+
+    # if opener == "file":
+    #     if syntax1 == "starred":
+     #       if syntax2 ==
